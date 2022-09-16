871f54ab38030901b9ba17bae94c588493a6aefe
jbradley84 <utbradley84@yahoo.com>
2022-09-16 12:57:01 -0500

    added basic Resume JSX

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 
-function Resume() {}
+function Resume() {
+   return (
+      <div>
+         <a href="https://docs.google.com/document/d/1N_NGuNebxqhGszZxj6Lz_lDJCXsL2d733uF-ldxDseM/edit?usp=sharing"
+         ><span>
+            i see you looking...</span>click here to view my resume</a>
+      </div>
+   )
+}
 
 export default Resume;
