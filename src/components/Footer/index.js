5bf5fc338d7dd1e90f4c08850b9dc15f431afcd3
jbradley84 <utbradley84@yahoo.com>
2022-09-16 11:49:00 -0500

    added basic footer JSX

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,14 @@
 import React from "react";
 
-function Footer() {}
+function Footer() {
+   return (
+      <footer>
+         <h4>made with ❤️ by J. Bradley Whittington</h4>
+        <p>
+            &copy; 2022 J. Bradley Whittington
+        </p>
+      </footer>
+   )
+}
 
 export default Footer;
